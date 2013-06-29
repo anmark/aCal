369bb2b606cb7f04126498308fbab50295adf9ec
Anmar Khazal <anmarkhazal@gmail.com>
2013-06-29 20:27:04 +0200

    Add optimizations, density independant pixel on textsize

--- src/com/anmark/calender/CalenderView.java
+++ src/com/anmark/calender/CalenderView.java
@@ -18,26 +18,33 @@ import android.view.View;
 public class CalenderView extends View {
 
 	private Calendar cal;
+	private Bitmap mOrigBitmap, mScaledBitmap;
 	private Paint paint;
+	private int mOrigBitmapWidth, mOrigBitmapHeight;
 
 	public CalenderView(Context context) {
-		super(context);	
-		init();
+		super(context);
+		init(context);
 	}
 
 	public CalenderView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		init();
+		init(context);
 	}
 
 	public CalenderView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		init();
+		init(context);
 	}
 
-	private void init() {
-		//TODO:
+	private void init(Context context) {
 		cal = Calendar.getInstance();
+
+		// Create original mutable bitmap from drawable resource
+		mOrigBitmap = BitmapFactory.decodeResource(context.getResources(),
+				R.drawable.calendar_sheet);
+		mOrigBitmapWidth = 0;
+		mOrigBitmapHeight = 0;
 	}
 
 	@SuppressLint("DrawAllocation")
@@ -45,95 +52,87 @@ public class CalenderView extends View {
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 
-		// Fetch mutable bitmap from drawable resource
-		Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(), R.drawable.calendar_sheet);
+		if (this.getHeight() != mOrigBitmapHeight
+				|| this.getWidth() != mOrigBitmapWidth) {
+			// Create scaled bitmap from original bitmap
+			mOrigBitmapHeight = this.getHeight();
+			mOrigBitmapWidth = this.getWidth();
+			mScaledBitmap = Bitmap.createScaledBitmap(mOrigBitmap,
+					mOrigBitmapWidth, mOrigBitmapHeight, true);
+
+			// Draw background bitmap
+			canvas.drawBitmap(mScaledBitmap, 0, 0, null);
+		} else {
+			canvas.drawBitmap(mOrigBitmap, null,
+					new Rect(0, 0, canvas.getWidth(), canvas.getHeight()),
+					paint);
+		}
 
 		// Setup paint
 		Paint paint = new Paint();
 		paint.setAntiAlias(true);
 		paint.setTextAlign(Align.CENTER);
 
-		//Draw background bitmap
-		canvas.drawBitmap(bitmap, null, new Rect(0,0, canvas.getWidth(), canvas.getHeight()), paint);
+		// Draw black bottom day name
+		paint.setTextSize(((canvas.getHeight()) / 180)
+				* getResources().getDimension(R.dimen.small_text_size));
+		canvas.drawText(getDayName(), canvas.getWidth() / 2f,
+				canvas.getHeight() * .9f, paint);
 
-		//Draw black bottom day name
-		paint.setTextSize((canvas.getWidth()+canvas.getHeight())/18);
-		canvas.drawText(getDayName(), canvas.getWidth()/2f, canvas.getHeight()*.9f, paint);
-
-		//Draw top white month name
+		// Draw top white month name
 		paint.setColor(Color.WHITE);
-		canvas.drawText(getMonthName(), canvas.getWidth()/2f, canvas.getHeight()*.4f, paint);
+		canvas.drawText(getMonthName(), canvas.getWidth() / 2f,
+				canvas.getHeight() * .4f, paint);
 
-		//Draw black center date
+		// Draw black center date
 		paint.setColor(Color.BLACK);
-		paint.setTextSize((canvas.getWidth()+canvas.getHeight())/10);
-		canvas.drawText(Integer.toString(getDayNumber()), canvas.getWidth()/2f, canvas.getHeight()*.75f, paint);
+		paint.setTextSize(((canvas.getHeight()) / 100)
+				* getResources().getDimension(R.dimen.small_text_size));
+		canvas.drawText(Integer.toString(getDayNumber()),
+				canvas.getWidth() / 2f, canvas.getHeight() * .72f, paint);
 
-		//Draw black vertical right year
-		paint.setTextSize((canvas.getWidth()+canvas.getHeight())/14);
-		canvas.rotate(90, canvas.getWidth()*.8f, canvas.getHeight()*.7f);
-		canvas.drawText(Integer.toString(getYear()), canvas.getWidth()*.8f, canvas.getHeight()*.7f, paint);
+		// Draw black vertical right year
+		paint.setTextSize(((canvas.getHeight()) / 180)
+				* getResources().getDimension(R.dimen.small_text_size));
+		canvas.rotate(90, canvas.getWidth() * .8f, canvas.getHeight() * .7f);
+		canvas.drawText(Integer.toString(getYear()), canvas.getWidth() * .8f,
+				canvas.getHeight() * .7f, paint);
 
-		//Bitmap bitmap = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.calendar_sheet);
-		//canvas.drawBitmap(bitmap, canvas.getMatrix(), paint);
-		//Bitmap bitmapScaled = Bitmap.createScaledBitmap(bitmap, canvas.getWidth(), canvas.getHeight(), true);
-		//bitmap.recycle();
-		//Bitmap convertedBitmap = convertToMutable(bitmap);
-
-		//canvas.drawBitmap(bitmap, null, new Rect(0,0,getWidth(), getHeight()), paint);
-
-		//canvas.drawText(getDayName(), getWidth()/2, getHeight()/2, paint);
-
-		/*	@SuppressWarnings("deprecation")
-		Drawable drawable = new BitmapDrawable(bitmapScaled);
-
-		image.setBackgroundDrawable(drawable);
-		 */
-		//canvas.drawBitmap(bitmap, null, null, paint);
-
-		//Bitmap background = Bitmap.createScaledBitmap(bitmap, this.getWidth(), this.getHeight(), false);
-
-		//canvas.drawBitmap(bitmap, 0, 0, null);
-
-		//canvas.drawBitmap(bitmap,  bitmap.getWidth() - (bitmap.getWidth() / 2), bitmap.getHeight() - (bitmap.getHeight() / 2), null);
-
-		//canvas.drawBitmap(bitmap, bitmap.getWidth(), bitmap.getHeight(), paint);
-
-		//canvas.drawBitmap(bitmap);
-
-		// Save canvas state
-		//canvas.save();
-		// Every save() must be matched with restore()
-		//canvas.restore();
-		
 		// Best practice to invalidate here?
-		postInvalidate();
+		// postInvalidate();
 	}
-	public int getYear(){
+
+	public int getYear() {
 		Integer year = cal.get(Calendar.YEAR);
 		return year;
 	}
-	public String getMonthName(){
+
+	public String getMonthName() {
 		String monthNames[] = new DateFormatSymbols().getMonths();
-		String month = monthNames[cal.get(Calendar.MONTH)];		 
-		return month = Character.toUpperCase(month.charAt(0)) + month.substring(1);
+		String month = monthNames[cal.get(Calendar.MONTH)];
+		return month = Character.toUpperCase(month.charAt(0))
+				+ month.substring(1);
 	}
 
-	public String getDayName(){
+	public String getDayName() {
 		String dayNames[] = new DateFormatSymbols().getWeekdays();
 		String day = dayNames[cal.get(Calendar.DAY_OF_WEEK)];
 		return day = Character.toUpperCase(day.charAt(0)) + day.substring(1);
 	}
-	public int getDayNumber(){
+
+	public int getDayNumber() {
 		Integer dayNumber = cal.get(Calendar.DATE);
 		return dayNumber;
-	}	
-	public int nextDay(){
+	}
+
+	public int nextDay() {
 		cal.add(Calendar.DATE, 1);
+		invalidate();
 		return getDayNumber();
 	}
 
-	public void setDate(Calendar cal){
+	public void setDate(Calendar cal) {
+		invalidate();
 		this.cal = cal;
 	}
 
@@ -145,65 +144,49 @@ public class CalenderView extends View {
 		return newCal;
 	}
 
-
 	/*
-	/**
-	 * Converts a immutable bitmap to a mutable bitmap. This operation doesn't allocates
-	 * more memory that there is already allocated.
+	 * /** Converts a immutable bitmap to a mutable bitmap. This operation
+	 * doesn't allocates more memory that there is already allocated.
 	 * 
-	 * @param imgIn - Source image. It will be released, and should not be used more
+	 * @param imgIn - Source image. It will be released, and should not be used
+	 * more
+	 * 
 	 * @return a copy of imgIn, but muttable.
-	 *
-	public static Bitmap convertToMutable(Bitmap imgIn) {
-		try {
-			//this is the file going to use temporally to save the bytes. 
-			// This file will not be a image, it will store the raw image data.
-			File file = new File(Environment.getExternalStorageDirectory() + File.separator + "temp.tmp");
-
-			//Open an RandomAccessFile
-			//Make sure you have added uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"
-			//into AndroidManifest.xml file
-			RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
-
-			// get the width and height of the source bitmap.
-			int width = imgIn.getWidth();
-			int height = imgIn.getHeight();
-			Config type = imgIn.getConfig();
-
-			//Copy the byte to the file
-			//Assume source bitmap loaded using options.inPreferredConfig = Config.ARGB_8888;
-			FileChannel channel = randomAccessFile.getChannel();
-			MappedByteBuffer map = channel.map(MapMode.READ_WRITE, 0, imgIn.getRowBytes()*height);
-			imgIn.copyPixelsToBuffer(map);
-			//recycle the source bitmap, this will be no longer used.
-			imgIn.recycle();
-			System.gc();// try to force the bytes from the imgIn to be released
-
-			//Create a new bitmap to load the bitmap again. Probably the memory will be available. 
-			imgIn = Bitmap.createBitmap(width, height, type);
-			map.position(0);
-			//load it back from temporary 
-			imgIn.copyPixelsFromBuffer(map);
-			//close the temporary file and channel , then delete that also
-			channel.close();
-			randomAccessFile.close();
-
-			// delete the temp file
-			file.delete();
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} 
-
-		return imgIn;
-	}
+	 * 
+	 * public static Bitmap convertToMutable(Bitmap imgIn) { try { //this is the
+	 * file going to use temporally to save the bytes. // This file will not be
+	 * a image, it will store the raw image data. File file = new
+	 * File(Environment.getExternalStorageDirectory() + File.separator +
+	 * "temp.tmp");
+	 * 
+	 * //Open an RandomAccessFile //Make sure you have added uses-permission
+	 * android:name="android.permission.WRITE_EXTERNAL_STORAGE" //into
+	 * AndroidManifest.xml file RandomAccessFile randomAccessFile = new
+	 * RandomAccessFile(file, "rw");
+	 * 
+	 * // get the width and height of the source bitmap. int width =
+	 * imgIn.getWidth(); int height = imgIn.getHeight(); Config type =
+	 * imgIn.getConfig();
+	 * 
+	 * //Copy the byte to the file //Assume source bitmap loaded using
+	 * options.inPreferredConfig = Config.ARGB_8888; FileChannel channel =
+	 * randomAccessFile.getChannel(); MappedByteBuffer map =
+	 * channel.map(MapMode.READ_WRITE, 0, imgIn.getRowBytes()*height);
+	 * imgIn.copyPixelsToBuffer(map); //recycle the source bitmap, this will be
+	 * no longer used. imgIn.recycle(); System.gc();// try to force the bytes
+	 * from the imgIn to be released
+	 * 
+	 * //Create a new bitmap to load the bitmap again. Probably the memory will
+	 * be available. imgIn = Bitmap.createBitmap(width, height, type);
+	 * map.position(0); //load it back from temporary
+	 * imgIn.copyPixelsFromBuffer(map); //close the temporary file and channel ,
+	 * then delete that also channel.close(); randomAccessFile.close();
+	 * 
+	 * // delete the temp file file.delete();
+	 * 
+	 * } catch (FileNotFoundException e) { e.printStackTrace(); } catch
+	 * (IOException e) { e.printStackTrace(); }
+	 * 
+	 * return imgIn; }
 	 */
 }
-
-
-
-
-
-
